aadb1d4bc2d83e76a4114831863432001527b3d7
Amit Prajapati <amitprj737@gmail.com>
2026-09-02 20:38:51 +0530

    theme setup and bug fix shadcn

--- platform-admin/src/App.tsx
+++ platform-admin/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
-   <div>
-      Platform Admin use React
+   <div className='font-bold text-green-600 text-2xl'>
+      Platform Admin
+
+      <Button>Click Me</Button>
    </div>
   )
 }
